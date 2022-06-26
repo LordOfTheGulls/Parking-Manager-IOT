e12c2bf017fdf78b325446f07014ed8616c6170d
Aleksandar Gospodinov <galeksanderg@gmail.com>
2022-06-27 02:16:04 +0300

    Improvements

--- barriers.py
+++ barriers.py
@@ -13,10 +13,11 @@ from functools import partial
 import pytesseract
 import imutils
 import re
+import random
 
 isParkingOpen = True
 
-entrance_boxes, exit_boxes = pd.DataFrame(), pd.DataFrame()
+entrance_boxes = pd.DataFrame()
 
 def handler_stop_signals(signum, frame):
     global isParkingOpen
@@ -26,16 +27,21 @@ signal.signal(signal.SIGINT, handler_stop_signals)
 signal.signal(signal.SIGTERM, handler_stop_signals)
 
 def entrance_cam_callback(request):
-    with MappedArray(request, "main") as im:
-        for idx, rect in entrance_boxes.iterrows():
-            cv2.rectangle(im.array, (math.floor(rect['xmin']), math.floor(rect['ymin'])), (math.floor(rect['xmax']), math.floor(rect['ymax'])), (0, 255, 0, 0))
+    if request is not None:
+        with MappedArray(request, "main") as im:
+            for idx, rect in entrance_boxes.iterrows():
+                cv2.rectangle(im.array, (math.floor(rect['xmin']), math.floor(rect['ymin'])), (math.floor(rect['xmax']), math.floor(rect['ymax'])), (0, 255, 0, 0))
 
 def start_barriers(neuralNet, showPreview: bool):
     try:
-        licensePlateRegex = re.compile(r'([0-9a-zA-Z- ]+)', re.DOTALL)
+        licensePlateRegex = re.compile(r'([0-9a-zA-Z-]+)', re.DOTALL)
 
         entrance_cam = Picamera2()
-        entrance_cam_config = entrance_cam.video_configuration(main={"format": 'XRGB8888', "size": (1280, 720)}, buffer_count=1)    
+        
+        entrance_cam.start_preview(Preview.QT)
+        
+        entrance_cam_config = entrance_cam.preview_configuration(main={"format": 'XRGB8888', "size": (720, 720)}, buffer_count=1)    
+        
         entrance_cam.configure(entrance_cam_config)
 
         entrance_cam.post_callback = entrance_cam_callback
@@ -47,6 +53,8 @@ def start_barriers(neuralNet, showPreview: bool):
         entrance_cam.start()
         
         while isParkingOpen:
+            print('Looking for detection: ', random.random())
+            
             entrance_boxes = pd.DataFrame()
             
             entranceImg = entrance_cam.capture_array()
@@ -58,9 +66,11 @@ def start_barriers(neuralNet, showPreview: bool):
             results = detection.pandas().xyxy[0]
 
             if len(results) > 0:
-                entrance_boxes = pd.DataFrame(results.head(1))
+                #BoundingBox Stage.
+                entrance_boxes = pd.DataFrame(results)
+                entrance_box = results.head(1)
                 #Preprocessing Stage.
-                cropped = entranceImg[math.floor(entrance_boxes['ymin']):math.floor(entrance_boxes['ymax']), math.floor(entrance_boxes['xmin']):math.floor(entrance_boxes['xmax'])]
+                cropped = entranceImgRGB[math.floor(entrance_box['ymin']):math.floor(entrance_box['ymax']), math.floor(entrance_box['xmin']):math.floor(entrance_box['xmax'])]
                 cropped_resized = imutils.resize(cropped, width=416)
                 cropped_gray = cv2.cvtColor(cropped_resized, cv2.COLOR_BGR2GRAY)
                 cropped_equalized = cv2.equalizeHist(cropped_gray)
@@ -68,12 +78,12 @@ def start_barriers(neuralNet, showPreview: bool):
                 cropped_binary = cv2.threshold(cropped_denoised, 0, 255, cv2.THRESH_OTSU)[1]
                 
                 licensePlateTxt = pytesseract.image_to_string(cropped_binary, lang='eng', config='--psm 7 --oem 3')
-
                 licensePlateMatch = re.search(licensePlateRegex, licensePlateTxt)
 
                 if licensePlateMatch is not None:
                     print('License Plate is: ', licensePlateMatch.group())
-
+             
+              
         #entrance_cam.stop_encoder()
         entrance_cam.stop()
 
